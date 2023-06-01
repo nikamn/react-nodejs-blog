38f52d6ba85ab312be8a61002793f920bd484e8b
Nishant Nikam <nikamn@gmx.com>
2023-06-01 17:03:23 +0530

    added dependencies and components, pages folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">React Node.js Blog</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
